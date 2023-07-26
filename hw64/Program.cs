a368d3a3d55bda28dad8271401d004b763b0fc89
A_SH <hciveegress@mail.ru>
2023-07-26 23:55:03 +0300

    hw64

---
+++ hw64/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+using System;
+using static System.Console;
+Clear();
+Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int summa(int m, int n)
+{
+if (m==n)
+return n;
+else return m + summa(m + 1, n);
+}
+Write($"Сумма натуральных элементов в промежутке от M до N равно {summa(m,n)}");
